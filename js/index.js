9396fa4fd7072a5590817483b1c33519a5ea0356
xin <freedomspace_xin@163.com>
2019-04-19 11:55:22 +0800

    TIM init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by xin on 2018-09-23.
+ */
+/*视差效果
+* 1、引入文件
+* 2、thml结构
+* 3、js初始化*/
+$(function($){
+    $.stellar({
+        horizontalScrolling: false,
+        responsive: true
+    });
+});
